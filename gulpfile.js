1b48d3589e4a1527267064b3b49b46b9f13a2975
softdyne01 <softdyne@daum.net>
2015-06-04 07:26:33 +0900

    jsdoc 작업 추가

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var gulp = require('gulp');
+var shell = require('gulp-shell');
 
 var env = process.env.NODE_ENV || 'development';
 /*
@@ -15,3 +16,12 @@ gulp.task('default', ['clean'], function (defaultTasks) {
   // run with paramater
   gulp.start(env);
 });
+
+// 문서 작업
+gulp.task('docs', shell.task([
+  'node_modules/jsdoc/jsdoc.js '+
+    '-c node_modules/angular-jsdoc/conf.json '+   // config file
+    '-t node_modules/angular-jsdoc/template '+    // template file
+    '-d build/docs '+                             // output directory
+    '-r packages/custom/theme/public/controllers'                              // source code directory
+]));
--- packages/custom/theme/public/controllers/theme.js
+++ packages/custom/theme/public/controllers/theme.js
@@ -1,5 +1,16 @@
 'use strict';
 
+/**
+* @namespace mean.theme
+* @author 신창현
+* @copyright softdyne
+*/
+
+/**
+* @class ThemeController
+* @memberOf mean.theme
+* @description ThemeController 데이터 추가
+*/
 angular.module('mean.theme')
 	.controller('ThemeController', ['$scope', 'Global',
 	  function($scope, Global) {
